f6ce37e1bf64846ea3a5da8227f9aeda902f0f40
Eddie Cheng <schaffenneu47@gmail.com>
2023-06-15 13:08:54 +0800

    countBitHammingWeight

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ function calSimpleInterest(deposit, interestRate, years) {
 function calCompoundInterest(deposit, interestRate, years) {
     return deposit * Math.pow((1 + interestRate), years) - deposit;
 }
+
+/**
+ * Count the number of bit 1 in a number.
+ * 
+ * @param {number} num - The number to be counted.
+ * @param {number} bit - The bit of the number.
+ * @return {number} The number of bit 1.
+ */
+function countBitHammingWeight(num, bit) {
+    let count = 0;
+    const mask = 1;
+    for (i = 0; i < bit; i++) {
+      if ((mask & num) != 0) {
+        count++;
+      }
+      num >>= 1;
+    }
+    return count;
+  }
